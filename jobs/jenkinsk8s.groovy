b32dd6dc9ddcd546ea989fd40d2c986e8be625b0
Henry Jao <henry.jao@grandsys.com>
2017-05-03 13:54:55 +0800

    change scm to gitlab

--- jobs/jenkinsk8s.groovy
+++ jobs/jenkinsk8s.groovy
@@ -1,10 +1,12 @@
-def auther = 'jaohaohsuan'
+def auther = 'henry.jao'
 def proj   = 'jenkins-kubernetes'
 
 multibranchPipelineJob(proj) {
   branchSources {
     git {
-      remote("https://github.com/${auther}/${proj}.git")
+      remote("ssh://git@rd.grandsys.com:8687/${auther}/${proj}.git")
+      credentialsId('henry-gitlab')
+      includes('*')
     }
   }
   orphanedItemStrategy {
