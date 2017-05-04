31212bb145bba1a731808d2e58c7c26f54c4e91b
Henry Jao <jaohaohsuan@gmail.com>
2017-05-04 15:45:32 +0800

    change credentialsId

--- jobs/jenkinsk8s.groovy
+++ jobs/jenkinsk8s.groovy
@@ -5,7 +5,7 @@ multibranchPipelineJob(proj) {
   branchSources {
     git {
       remote("ssh://git@rd.grandsys.com:8687/${auther}/${proj}.git")
-      credentialsId('henry-gitlab')
+      credentialsId('gitlab-inu')
       includes('*')
     }
   }
